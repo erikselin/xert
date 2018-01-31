bf71436a6c166e8f435d7d650ef59a5d6c98201e
Erik Selin <erik.selin@gmail.com>
2018-01-30 20:04:40 -0500

    remove examples for now

--- examples/sort/reduce.py
+++
@@ -1,6 +0,0 @@
-from sys import stdin, stdout
-
-
-if __name__ == "__main__":
-    for line in stdin:
-        stdout.write(line)
--- examples/string_search/map.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin, stdout
-
-if __name__ == "__main__":
-    for line in stdin:
-        _, value = line.split('\t')
-        if value.startswith('aaaaa'):
-            stdout.write(line)
